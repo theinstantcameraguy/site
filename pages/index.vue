e0720e83942d3956f079218a5e0a7d5e8d3cad5e
Cameron Poole <Cameron.j.poole@gmail.com>
2019-04-18 08:38:52 +1000

    Fix initial page

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,23 @@
 <template>
   <section class="container">
     <div>
-      <logo />
       <h1 class="title">
-        site
+        The Instant Camera Guy
       </h1>
       <h2 class="subtitle">
-        Site for The Instant Camera Guy (Jake Threepwood)
+        Coming Soon
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
     </div>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<!--<script>-->
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+<!--// export default {-->
+<!--//   components: {-->
+<!--//   }-->
+<!--// }-->
+<!--</script>-->
 
 <style>
 .container {
