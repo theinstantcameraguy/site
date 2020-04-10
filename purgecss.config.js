30e21c9f2f810027d13175acd0f896c42a3de13d
Cameron Poole <Cameron.j.poole@gmail.com>
2020-04-10 12:22:30 +0100

    Fix purge css

--- purgecss.config.js
+++ purgecss.config.js
@@ -21,6 +21,11 @@ module.exports = {
     'b-carousel',
     'b-carousel-item'
   ],
+  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/ ],
+  defaultExtractor (content) {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
   extractors: [
     {
       extractor: content => content.match(/[A-z0-9-:\\/]+/g),
